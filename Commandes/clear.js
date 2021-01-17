2dbef8489d4ba811393e4d24a670337c0e857994
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-01-17 17:39:19 +0100

    improve the code

--- Commandes/clear.js
+++ Commandes/clear.js
@@ -1,9 +1,7 @@
-const Discord = require("discord.js");
-
 module.exports.run = (client, message, args) => {
-    if (!message.guild.member(message.author).hasPermission('MANAGE_MESSAGES')) { return message.channel.send('Vous n\'avez pas les permissions !'); }
+    if (!message.guild.member(message.author).hasPermission('MANAGE_MESSAGES')) { return message.channel.send('Vous n\'avez pas les permissions !'); };
     if (!args[0]) { return message.channel.send('Vous devez spécifier un nombre de messages à supprimer !'); }
-    else if (isNaN(args[0])) { return message.channel.send('Veuillez spécifier un nombre !'); }
+    else if (isNaN(args[0])) { return message.channel.send('Veuillez spécifier un nombre !'); };
 
         message.channel.bulkDelete(args[0])
             .then((messages) => {
@@ -12,5 +10,5 @@ module.exports.run = (client, message, args) => {
 };
 
 module.exports.help = {
-    name: 'clear'
+    name: 'clear',
 };
--- Commandes/kick.js
+++ Commandes/kick.js
@@ -1,12 +1,10 @@
-const Discord = require('discord.js');
-
-module.exports.run = (client, message, args) => {
-    if (!message.guild.member(message.author).hasPermission('KICK_MEMBERS')) { return message.channel.send('Vous n\'avez pas la permission !'); }
-    if (!message.guild.member(client.user).hasPermission('KICK_MEMBERS')) { return message.channel.send('Le bot n\'a pas la permission !'); }
+module.exports.run = (client, message) => {
+    if (!message.guild.member(message.author).hasPermission('KICK_MEMBERS')) { return message.channel.send('Vous n\'avez pas la permission !'); };
+    if (!message.guild.member(client.user).hasPermission('KICK_MEMBERS')) { return message.channel.send('Le bot n\'a pas la permission !'); };
     if (message.mentions.users.size === 0) { return message.channel.send('Vous devez mentionner un utilisateur !'); }
 
         let kickMember = message.guild.member(message.mentions.users.first());
-        if (!kickMember) { return message.channel.send('Je n\'ai pas trouvé l\'utilisateur !'); }
+        if (!kickMember) { return message.channel.send('Je n\'ai pas trouvé l\'utilisateur !'); };
     
         message.mentions.users.first().send(`Vous êtes kick du serveur **${message.guild.name}** par ${message.author.username}`)
             .then(() => {
@@ -15,21 +13,21 @@ module.exports.run = (client, message, args) => {
                         message.channel.send(`${member.user.username} est kick ! Par ${message.author.username}`);
                     })
                         .catch((err) => {
-                            if (err) { return console.error(err); }
+                            if (err) { return console.log(err); };
                         });
             })
                 .catch((error) => {
-                    if (error) { console.error(error); }
+                    if (error) { console.log(error); };
                         kickMember.kick()
                             .then((member) => {
                                 message.channel.send(`${member.user.username} est kick ! Par ${message.author.username}`);
                             })
                                 .catch((err) => {
-                                    if (err) { return console.error(err); }
+                                    if (err) { return console.log(err); };
                                 });
                 });
 };
 
 module.exports.help = {
-    name: 'kick'
+    name: 'kick',
 };
