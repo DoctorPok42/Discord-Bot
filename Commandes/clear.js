9510d002618bae5656e303cdc1410361a3d7b365
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2021-02-12 11:00:07 +0100

    Update clear.js

--- Commandes/clear.js
+++ Commandes/clear.js
@@ -1,5 +1,5 @@
 module.exports.run = (client, message, args) => {
-    if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) { return message.channel.send("Vous n\'avez pas les permissions !"); }
+    if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) { return message.channel.send('Vous n\'avez pas les permissions !'); }
     if (!args[0]) { return message.channel.send("Vous devez spécifier un nombre de messages à supprimer !"); }
     else if (isNaN(args[0])) { return message.channel.send("Veuillez spécifier un nombre !"); }
 
