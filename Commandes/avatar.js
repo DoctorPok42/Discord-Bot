ccd2569ccb11930e7a8eb07d9fa1e75faf0c06c2
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-02-16 18:44:33 +0100

    avatar.js

---
+++ Commandes/avatar.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports.run = (client, message, args) => {
+    let member = message.member;
+    if (args[0]) member = message.guild.member(message.mentions.users.first());
+    let user = member.user;
+    var avatar = new MessageEmbed()
+    .setColor(client.color.blue)
+    .setTimestamp()
+    .setDescription(`**Avatar de ${user}** \n **[.png](${user.displayAvatarURL({ format: "png", size: 512 })}) | [.jpg](${user.displayAvatarURL({ format: "jpg", size: 512 })}) | [.webp](${user.displayAvatarURL({ format: "webp", size: 512})}) | [.gif](${user.displayAvatarURL({ format: "gif", size: 512})})**`)
+    .setImage(user.displayAvatarURL())
+    message.channel.send(avatar)
+    console.log(`Demande d'avatar de la par de ${message.author.tag}`)
+}
+
+module.exports.help = {
+    name: "avatar"
+};
