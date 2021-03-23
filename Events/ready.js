5b91e9603e7b18534ce71db4627cc37e566b3168
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-03-23 18:44:43 +0100

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,7 +1,7 @@
 const moment = require("moment");
 
-module.exports = client => {
-  moment.locale('fr');
+module.exports = (client) => {
+  moment.locale("fr");
   console.log(`${client.user.tag} observe les ${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)} utilisateurs et ${client.guilds.cache.size} serveurs`);
-  client.channels.cache.get('YOUR CHANNEL PRIVATE ID').send(`Le bot est opérationnel ! (${client.guilds.cache.size})\nLe ${moment(client.online).format('LLL')}`);
+  client.channels.cache.get("YOUR CHANNEL PRIVATE ID").send(`Le bot est opérationnel ! (${client.guilds.cache.size})\nLe ${moment(client.online).format("LLL")}`);
 }
