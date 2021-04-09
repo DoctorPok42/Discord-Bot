cd87b784e7bbefa7dd58c33cb21eb25d75647656
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-04-09 21:19:03 +0200

    Create github.js

---
+++ Commandes/github.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require("discord.js");
+const { MESSAGES } = require("../../util/constants"),
+  fetch = require("node-fetch");
+
+module.exports.run = async (client, message, args) => {
+  const res = await fetch(
+    "https://api.github.com/repos/DoctorPok42/Moderation-Bot"
+  );
+  const json = await res.json();
+
+  const embed = new MessageEmbed()
+    .setAuthor(client.user.tag, client.user.displayAvatarURL())
+    .setDescription(
+      "[" + "CLICK_HERE" + "](https://github.com/DoctorPok42/Moderation-Bot)"
+    )
+    .addField("Stars", json.stargazers_count, true)
+    .addField("Forks", json.forks_count, true)
+    .addField("LANGUAGE", json.language, true)
+    .addField(
+      "OWNER",
+      "[" + json.owner.login + "](" + json.owner.html_url + ")"
+    )
+    .setImage(json.owner.avatar_url);
+
+  message.channel.send(embed);
+};
+
+module.exports.help = {
+  name: "github",
+};
